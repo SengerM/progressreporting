16c22a9be84a433f031028e4d268c4333a58ea0b
Matias Senger <m.senger@hotmail.com>
2021-08-21 00:09:34 +0200

    - Implemented "main class" TelegramReporter, now TelegramProgressReporter is a sub class of this one. - TelegramProgressReporter: Forcing that methods should be called inside a `with` statement with a specific attribute checking for this.

--- progressreporting/TelegramProgressReporter.py
+++ progressreporting/TelegramProgressReporter.py
@@ -6,36 +6,118 @@ try:
 except ImportError:
 	raise ImportError('You need the "humanize" package, just run "pip install humanize".')
 
-class TelegramProgressReporter:
-	"""
-	Usage example
-	-------------
-	
-	from progressreporting.TelegramProgressReporter import TelegramProgressReporter
-	import time
-
-	BOT_TOKEN = 'Token of your bot'
-	CHAT_ID = 'ID of the chat to which you want to send the updates'
-
-	MAX_K = 99999
-
-	with TelegramProgressReporter(MAX_K, BOT_TOKEN, CHAT_ID, 'This is a long loop') as reporter:
-		for k in range(MAX_K):
-			print(k)
-			time.sleep(0.01)
-			reporter.update(1)
-	
-	"""
-	def __init__(self, total: int, telegram_token: str, telegram_chat_id: str, loop_name=None, miminum_update_time_seconds=60, minimum_warn_time_seconds=60*5):
+class TelegramReporter:
+	def __init__(self, telegram_token: str, telegram_chat_id: str):
 		self._telegram_token = telegram_token
 		self._telegram_chat_id = telegram_chat_id
+		self._session = requests.Session() # https://stackoverflow.com/questions/25239650/python-requests-speed-up-using-keep-alive
+	
+	def send_message(self, message_text, reply_to_message_id=None):
+		"""Send a message in a safe manner, in the sense that this method
+		will not raise any error at all. In case it cannot send the message
+		e.g. because the internet connection is temporarily down it will
+		only display a warning."""
+		try:
+			return self._send_message(str(message_text), reply_to_message_id)
+		except KeyboardInterrupt:
+			# If someone presses "ctrl+c" it is because he wants to interrupt the program.
+			raise KeyboardInterrupt()
+		except Exception as e:
+			# Any other exception that may happen due to sending the message (e.g. internet connection is out) we don't want to crash the program.
+			warnings.warn(f'Could not send message to Telegram, reason: {repr(e)}.')
+	
+	def edit_message(self, message_text, message_id):
+		"""Edit a message in a safe manner, in the sense that this method
+		will not raise any error at all. In case it cannot send the message
+		e.g. because the internet connection is temporarily down it will
+		only display a warning."""
+		try:
+			return self._edit_message(str(message_text), message_id)
+		except KeyboardInterrupt:
+			# If someone presses "ctrl+c" it is because he wants to interrupt the program.
+			raise KeyboardInterrupt()
+		except Exception as e:
+			# Any other exception that may happen due to sending the message (e.g. internet connection is out) we don't want to crash the program.
+			warnings.warn(f'Could not send message to Telegram, reason: {repr(e)}.')
+	
+	def _send_message(self, message_text, reply_to_message_id=None):
+		"""This is the method that actually sends the messages. It is not
+		safe in the sense that if it cannot send the message it will raise
+		an error, making you responsible of handling this error."""
+		# https://core.telegram.org/bots/api#sendmessage
+		parameters = {
+				'chat_id': self._telegram_chat_id,
+				'text': message_text,
+			}
+		if reply_to_message_id is not None:
+			parameters['reply_to_message_id'] = str(int(reply_to_message_id))
+		response = self._session.get(
+			f'https://api.telegram.org/bot{self._telegram_token}/sendMessage',
+			data = parameters,
+			timeout = 1, # https://stackoverflow.com/a/21966169/8849755
+		)
+		return response.json()
+
+	def _edit_message(self, message_text, message_id):
+		"""This is the method that actually edits the messages. It is not
+		safe in the sense that if it cannot send the message it will raise
+		an error, making you responsible of handling this error."""
+		# https://core.telegram.org/bots/api#editmessagetext
+		self._session.post(
+			f'https://api.telegram.org/bot{self._telegram_token}/editMessageText',
+			data = {
+				'chat_id': self._telegram_chat_id,
+				'text': message_text,
+				'message_id': str(message_id),
+			},
+			timeout = 1, # https://stackoverflow.com/a/21966169/8849755
+		)
+	
+	def report_for_loop(self, total, loop_name=None, miminum_update_time_seconds=60, minimum_warn_time_seconds=60):
+		"""Creates an instance of TelegramProgressReporter and returns it
+		to use inside a `with` statement, using the same Telegram token
+		and chat id."""
+		return TelegramProgressReporter(
+			total = total, 
+			telegram_token = self._telegram_token, 
+			telegram_chat_id = self._telegram_chat_id, 
+			loop_name = loop_name, 
+			miminum_update_time_seconds = miminum_update_time_seconds, 
+			minimum_warn_time_seconds = minimum_warn_time_seconds,
+		)
+
+class TelegramProgressReporter(TelegramReporter):
+	def __init__(self, total: int, telegram_token: str, telegram_chat_id: str, loop_name=None, miminum_update_time_seconds=60, minimum_warn_time_seconds=60):
+		"""
+		Usage example
+		-------------
+		
+		from progressreporting.TelegramProgressReporter import TelegramProgressReporter
+		import time
+
+		BOT_TOKEN = 'Token of your bot'
+		CHAT_ID = 'ID of the chat to which you want to send the updates'
+
+		MAX_K = 99999
+
+		with TelegramProgressReporter(MAX_K, BOT_TOKEN, CHAT_ID, 'This is a long loop') as reporter:
+			for k in range(MAX_K):
+				print(k)
+				time.sleep(0.01)
+				reporter.update(1)
+		
+		"""
+		super().__init__(
+			telegram_token = telegram_token,
+			telegram_chat_id = telegram_chat_id,
+		)
 		self._title = loop_name if loop_name is not None else ('loop started on ' + datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
 		if not isinstance(total, int):
 			raise TypeError(f'<total> must be an integer number, received {total} of type {type(total)}.')
 		self._total = total
-		self._session = requests.Session() # https://stackoverflow.com/questions/25239650/python-requests-speed-up-using-keep-alive
 		self._minimum_update_time = datetime.timedelta(seconds=float(miminum_update_time_seconds))
 		self._minimum_warn_time = datetime.timedelta(seconds=float(minimum_warn_time_seconds))
+		self._within_context = False
 	
 	@property
 	def expected_finish_time(self):
@@ -49,6 +131,7 @@ class TelegramProgressReporter:
 			self._message_id = response['result']['message_id']
 		except Exception as e:
 			warnings.warn(f'Could not establish connection with Telegram to send the progress status. Reason: {repr(e)}')
+		self._within_context = True
 		return self
 		
 	def __exit__(self, exc_type, exc_value, exc_traceback):
@@ -74,48 +157,24 @@ class TelegramProgressReporter:
 				)
 			except Exception as e:
 				warnings.warn(f'Could not establish connection with Telegram to send the progress status. Reason: {repr(e)}')
-	
-	def send_message(self, message_text, reply_to_message_id=None):
-		# https://core.telegram.org/bots/api#sendmessage
-		parameters = {
-				'chat_id': self._telegram_chat_id,
-				'text': message_text,
-			}
-		if reply_to_message_id is not None:
-			parameters['reply_to_message_id'] = str(int(reply_to_message_id))
-		response = self._session.get(
-			f'https://api.telegram.org/bot{self._telegram_token}/sendMessage',
-			data = parameters,
-			timeout = 1, # https://stackoverflow.com/a/21966169/8849755
-		)
-		return response.json()
-
-	def edit_message(self, message_text, message_id):
-		# https://core.telegram.org/bots/api#editmessagetext
-		self._session.post(
-			f'https://api.telegram.org/bot{self._telegram_token}/editMessageText',
-			data = {
-				'chat_id': self._telegram_chat_id,
-				'text': message_text,
-				'message_id': str(message_id),
-			},
-			timeout = 1, # https://stackoverflow.com/a/21966169/8849755
-		)
+		self._within_context = False
 	
 	def set_completed(self):
-		if not hasattr(self, '_count'):
-			raise RuntimeError(f'Before calling to <set_completed> you should create a context using "with TelegramProgressBar(...) as reporter:".')
+		if self._within_context == False:
+			raise RuntimeError(f'This method must be called from inside a context, i.e. inside a `with` statement.')
 		self._count = self._total
 	
 	def count(self, count):
-		if not hasattr(self, '_count'):
-			raise RuntimeError(f'Before calling to <count> you should create a context using "with TelegramProgressBar(...) as reporter:".')
+		if self._within_context == False:
+			raise RuntimeError(f'This method must be called from inside a context, i.e. inside a `with` statement.')
 		self._count += count
 	
 	def report(self):
 		"""This is the method that actually sends a report to the Telegram
 		chat. Normally you should not call this method, unless for some 
 		reason you want to force the report."""
+		if self._within_context == False:
+			raise RuntimeError(f'This method must be called from inside a context, i.e. inside a `with` statement.')
 		message_string = f'{self._title}\n\n'
 		message_string += f'{self._start_time.strftime("%Y-%m-%d %H:%M")} | Started\n'
 		if self.expected_finish_time is not None:
@@ -153,10 +212,10 @@ class TelegramProgressReporter:
 		When calling this method, updates are automatically sent to the 
 		Telegram chat.
 		This method will send the warnings too, in case they are accumulated."""
-		if not hasattr(self, '_count'):
-			raise RuntimeError(f'Before calling to <update> you should create a context using "with TelegramProgressBar(...) as reporter:".')
+		if self._within_context == False:
+			raise RuntimeError(f'This method must be called from inside a context, i.e. inside a `with` statement.')
 		if not isinstance(count, int):
-			raise TypeError(f'<count> must be an integer number, received {count} of type {type(count)}.')
+			raise TypeError(f'<count> must be an integer number, received object of type {type(count)}.')
 		if not hasattr(self, '_last_update'):
 			self._last_update = datetime.datetime.now()
 		self.count(count)
@@ -175,6 +234,8 @@ class TelegramProgressReporter:
 		This avoids spamming the Telegram account and avoids the bot being
 		blocked by Telegram for "inhuman behavior".
 		Raises TypeError if <message> is not a string."""
+		if self._within_context == False:
+			raise RuntimeError(f'This method must be called from inside a context, i.e. inside a `with` statement.')
 		if not isinstance(message, str):
 			raise TypeError(f'<message> must be a string, received an object of type {type(message)}.')
 		if not hasattr(self, '_accumulated_warnings'):
